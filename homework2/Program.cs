2915cdde8b128f758e20275e4b6f1cf3bcc0da12
Alevtena <success_2006@mail.ru>
2023-07-09 17:17:35 +0300

    Добавила второй вариант решения первой задачи при генерации рандомного трехзначного числа

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -1,8 +1,8 @@
-﻿// Task 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿// Task 1. Вариант 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-
+/*
 int CutNumber (int number)
 {
 int hundres = number / 100;
@@ -10,6 +10,33 @@ int units = number % 10;
 int result = (number - hundres * 100 - units) / 10;
 return result;
 }
-int randNumber = new Random().Next(100,1000);
-int newNumber = CutNumber(randNumber);
-Console.WriteLine($"New version of {randNumber} is {newNumber}");
+Console.Write ("Input a three-digit number");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number<100 || Number > 1000) 
+{
+    Console.Write ("You are inputed not tree-digit number  ");
+}
+else 
+{int digit = CutNumber(Number);
+Console.WriteLine($"Second digit  of {Number} is {digit}");
+}
+*/
+
+//  Task 1. Вариант 2. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int CutNumber (int number)
+{
+int digit = number / 10 % 10;
+return digit;
+}
+
+int Number = new Random().Next (100,1000);
+Console.WriteLine  ($"Зададим случайное трехзначное число  {Number}");
+int digit = CutNumber(Number);
+Console.WriteLine($"Second digit  of {Number} is {digit}");
+
+
+// Task 2. Напишите программу, которая выводит третью цифру заданного числа.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
